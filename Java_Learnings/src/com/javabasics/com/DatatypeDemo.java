f6f8c1add1917125b42e38bdc09812fce146e189
dineshGitHub93 <62668115+dineshGitHub93@users.noreply.github.com>
2020-06-16 14:26:58 +0530

    Variables_Practice

---
+++ Java_Learnings/src/com/javabasics/com/DatatypeDemo.java
@@ -0,0 +1,45 @@
+package com.javabasics.com;
+
+public class DatatypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			//Below four data types to store integer values
+		
+			byte b = 78;
+			short s= 32767;
+			int i =2147483647;
+			long l = 9223372036854775807L;
+			
+			//Below two data types to store Floating values
+			
+			float f= 234.5f;
+			double d1 = 12.3;
+			
+			// Below one is check true or false;
+			
+			boolean bool = false;
+			
+			// Below one is to store char value
+			
+			char ch = 'a';
+			char ch1= (char)b;
+			
+			System.out.println(b);
+			System.out.println(s);
+			System.out.println(i);
+			System.out.println(l);
+			System.out.println(f);
+			System.out.println(d1);
+			System.out.println(bool);
+			System.out.println(ch);
+			System.out.println(ch1);
+			
+			
+			
+			
+
+	}
+
+}
---
+++ Java_Learnings/src/com/javabasics/com/VariableDemp.java
@@ -0,0 +1,25 @@
+package com.javabasics.com;
+
+public class VariableDemp {
+	
+	// Instance variable 
+	// Static -> Static variable
+	
+	public void myMethod() {
+		
+		// Local variable 
+		
+		if() {
+			//Local Variable
+		}
+		for() {
+			//Local variable 
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
