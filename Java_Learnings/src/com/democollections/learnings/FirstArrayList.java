ea1acfd069b6dc7cb1c75371ce9d38f6e136b8fa
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-07-21 23:16:52 +0530

    HashMap Demo

--- Java_Learnings/src/com/democollections/learnings/FirstArrayList.java
+++ Java_Learnings/src/com/democollections/learnings/FirstArrayList.java
@@ -11,6 +11,19 @@ public class FirstArrayList {
 		demo.ensureCapacity(8);
 		
 		System.out.println();
+		
+		WrittenArrayList<Integer> demo1 = new WrittenArrayList<Integer>();
+		
+		demo1.add(20);
+		demo1.add(10);
+		demo1.add(100);
+		demo1.add(90);
+		demo1.add(60);
+		
+		System.out.println(demo1.parallelStream());
+		
+		
+		
 	}
 
 }
