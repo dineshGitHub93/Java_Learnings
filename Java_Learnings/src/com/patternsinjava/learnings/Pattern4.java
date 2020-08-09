f961b66dbd22c082ac074a92c1f19e7e0fe004fb
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-09 23:55:24 +0530

    pattern4 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern4.java
@@ -0,0 +1,16 @@
+package com.patternsinjava.learnings;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(char ch='A'; ch<='E'; ch++) {
+			for(int j=1; j<=5;j++) {
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+	}
+
+}
