6dc5a7b02bed3522b8ea7e12ca38d2c4654f3386
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-10 23:27:12 +0530

    Pattern21 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern21.java
@@ -0,0 +1,15 @@
+package com.patternsinjava.learnings;
+
+public class Pattern21 {
+	
+	public static void main(String args[]) {
+		
+		for(int i=4;i>=0;i--) {
+			for(int j=0;j<=i;j++) {
+				System.out.print((char)(j+65)+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
