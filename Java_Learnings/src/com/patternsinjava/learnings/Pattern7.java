e0cec274d78035ace123c20b27ebf08592376142
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-10 22:33:04 +0530

    Patter7 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern7.java
@@ -0,0 +1,17 @@
+package com.patternsinjava.learnings;
+
+public class Pattern7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(char ch='A'; ch<='E';ch++) {
+			for(char j='A';j<='E';j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
