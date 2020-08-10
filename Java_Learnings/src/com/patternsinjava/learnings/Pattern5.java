98954e68e26f2054f03d2490385ddebbaed581d7
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-10 22:32:16 +0530

    Patter5 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern5.java
@@ -0,0 +1,30 @@
+package com.patternsinjava.learnings;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+			for(int j=1; j<=3;j++) {
+				
+				System.out.print(" "+j);
+			}
+			System.out.println();
+			
+			for(int j=4; j<=6;j++) {
+				
+				System.out.print(" "+j);
+			}
+			
+			System.out.println();
+			
+			for(int j=7; j<=9;j++) {
+	
+				System.out.print(" "+j);
+			}
+			
+	}
+
+}
