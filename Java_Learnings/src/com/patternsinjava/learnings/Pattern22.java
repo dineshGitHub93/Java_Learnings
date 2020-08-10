9a4041904cf75249109c99b1797e23a6635fc73a
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-10 23:49:45 +0530

    Pattern22 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern22.java
@@ -0,0 +1,16 @@
+package com.patternsinjava.learnings;
+
+public class Pattern22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5; i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
