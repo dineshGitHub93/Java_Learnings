5136a8a6e0759952f4b88db7eaaf33ec987379ec
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-11 21:50:18 +0530

    Patter26 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern26.java
@@ -0,0 +1,21 @@
+package com.patternsinjava.learnings;
+
+public class Pattern26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int n=5;
+			for(int i=1; i<=n;i++) {
+				for(int j=n-1; j>=i;j--) {
+					System.out.print("  ");
+				}
+				for(int k=1;k<=i;k++) {
+					System.out.print(" *");
+				}
+				System.out.println();
+			}
+			
+	}
+
+}
