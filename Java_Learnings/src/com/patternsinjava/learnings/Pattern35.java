98278171eea478e2cfba47864b3382d367cd680d
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-11 23:48:33 +0530

    Pattern35 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern35.java
@@ -0,0 +1,22 @@
+package com.patternsinjava.learnings;
+
+public class Pattern35 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n=5;
+		for(int i=n;i>=1;i--){
+			for(int j=n;j>=i;j--) {
+				System.out.print("  ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.print(" "+(char)(k+64));
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
