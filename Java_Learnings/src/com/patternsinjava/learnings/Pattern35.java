9d2563920f4de29b785e084a695928186f732df1
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-11 23:52:48 +0530

    Pattern35 Demo update

--- Java_Learnings/src/com/patternsinjava/learnings/Pattern35.java
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern35.java
@@ -8,7 +8,7 @@ public class Pattern35 {
 		int n=5;
 		for(int i=n;i>=1;i--){
 			for(int j=n;j>=i;j--) {
-				System.out.print("  ");
+				System.out.print(" @");
 			}
 			for(int k=1;k<=i;k++) {
 				System.out.print(" "+(char)(k+64));
