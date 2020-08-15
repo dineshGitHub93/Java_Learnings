bb87cd06b680a7f9651a6ede55b76516fd1b75e2
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-15 23:06:13 +0530

    Pattern41 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern41.java
@@ -0,0 +1,30 @@
+package com.patternsinjava.learnings;
+
+public class Pattern41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n =5;
+		int px =n;
+		int py =n;
+		
+		for(int i=1;i<=n;i++) {
+			for(int j=1; j<n*2; j++) {
+				if(j>=px && j<=py) {
+					System.out.print(" *");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+				px--;
+				py++;
+				System.out.println();
+			}
+			
+		}
+
+	}
+
+
