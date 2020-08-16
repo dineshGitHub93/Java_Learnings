cf289214845525c46f3a7ec946c85a4425daf432
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-16 20:23:26 +0530

    Pattern50 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern50.java
@@ -0,0 +1,21 @@
+package com.patternsinjava.learnings;
+
+public class Pattern50 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=5-1;j>=i;j--) {
+				System.out.print("  ");
+			}
+			for(int k=1; k<=i;k++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
