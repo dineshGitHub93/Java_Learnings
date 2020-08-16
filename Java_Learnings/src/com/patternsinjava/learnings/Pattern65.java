03284aa5b7e2812764764b4517e6507e16edaddf
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-16 20:48:13 +0530

    Pattern65 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern65.java
@@ -0,0 +1,26 @@
+package com.patternsinjava.learnings;
+
+public class Pattern65 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int m=1;
+		int n=5;
+		int p_height =n-1; 
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=p_height;j>=i;j--) {
+				System.out.print("  ");
+			}
+			for(int k=1;k<=m;k++) {
+				System.out.print(" "+(char)(k+64));
+			}
+			
+			System.out.println();
+			m+=2;
+		}
+
+	}
+
+}
