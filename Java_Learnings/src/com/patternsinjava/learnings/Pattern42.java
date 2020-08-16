6521b73d6284440558374f9c21378126172d4d0c
dineshGitHub93 <dineshspachaiyappan93@gmail.com>
2020-08-16 20:17:56 +0530

    Pattern42 Demo

---
+++ Java_Learnings/src/com/patternsinjava/learnings/Pattern42.java
@@ -0,0 +1,22 @@
+package com.patternsinjava.learnings;
+
+public class Pattern42 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=5;
+		int z=1;
+		for(int i=1;i<=n;i++) {
+			for(int j=n; j>i; j--) {
+				System.out.print("  ");
+			}
+			for(int k=1; k<=z; k++) {
+				System.out.print(" "+i);
+			}
+			z+=2;
+			System.out.println();
+		}
+	}
+
+}
